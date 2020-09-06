c524c0cef51458d4595f6e712bf9569fa9ddac19
Josué Gimenes <josuegalvares@gmail.com>
2020-09-06 14:47:54 -0300

    Schedule Creation

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -7,5 +7,5 @@ export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>
   findAllInMonthFromProvider(data: IFindAllInMonthFromProviderDTO): Promise<Appointment[]>
-  findAllInDayFromProvider(data: IFindAllInDayFromProviderDTO): Promise<Appointment>;
+  findAllInDayFromProvider(data: IFindAllInDayFromProviderDTO): Promise<Appointment[]>;
 }
