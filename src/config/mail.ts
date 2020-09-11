eabb869c52ee377b4c40cae3b9529ebd10730590
Josué Gimenes <josuegalvares@gmail.com>
2020-09-10 23:07:43 -0300

    Emails through Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'josuegalvares@gmail.com',
+      name: 'Josué Gimenes',
+    },
+  },
+} as IMailConfig;
