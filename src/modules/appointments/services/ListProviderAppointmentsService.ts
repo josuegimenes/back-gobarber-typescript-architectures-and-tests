8dbcfe5a4c092071c7819ffb9ad8659badc22ccf
Josué Gimenes <josuegalvares@gmail.com>
2020-09-20 17:16:01 -0300

    API tweaks

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   provider_id: string;
@@ -36,7 +37,7 @@ class ListProviderAppointmentsService {
 
       await this.cacheProvider.save(
         cacheKey,
-        appointments);
+        classToClass(appointments));
     }
 
     return appointments;
